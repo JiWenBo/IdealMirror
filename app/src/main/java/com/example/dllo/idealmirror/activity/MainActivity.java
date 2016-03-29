efaa2cf39188947b4be0dab86b952c7f312aaecb
dllo <Jwb9325@163.com>
2016-03-29 14:21:07 +0800

    分包完成

--- app/src/main/java/com/example/dllo/idealmirror/activity/MainActivity.java
+++ app/src/main/java/com/example/dllo/idealmirror/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.dllo.idealmirror;
+package com.example.dllo.idealmirror.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.dllo.idealmirror.R;
 
 public class MainActivity extends AppCompatActivity {
 
