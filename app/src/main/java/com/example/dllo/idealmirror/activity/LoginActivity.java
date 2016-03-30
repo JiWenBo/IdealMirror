aa1c3aa386791e8bc91b17dd8aa83ebaf683f7dd
冀文博 <Jwb9325@163.com>
2016-03-30 14:19:13 +0800

    解决冲突

---
+++ app/src/main/java/com/example/dllo/idealmirror/activity/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.example.dllo.idealmirror.activity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.dllo.idealmirror.R;
+
+/**
+ * Created by LYH on 16/3/30.
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_login);
+    }
+}
