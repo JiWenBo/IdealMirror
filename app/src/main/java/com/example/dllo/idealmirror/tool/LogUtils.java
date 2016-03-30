4c0f8fdba28d059ab363c1f22b067451f7ee91a3
王亚男 <1104352824@qq.com>
2016-03-30 19:58:26 +0800

    viewpager冲突

---
+++ app/src/main/java/com/example/dllo/idealmirror/tool/LogUtils.java
@@ -0,0 +1,67 @@
+package com.example.dllo.idealmirror.tool;
+
+import android.util.Log;
+
+/**
+ * Created by nan on 16/3/30.
+ */
+public class LogUtils {
+    public static final boolean DEBUG = true;
+    private static final String TAG = "IdealMirror";
+
+    private LogUtils() {
+    }
+
+    // 使用默认的TAG
+    public static void i(String msg) {
+        if (DEBUG)
+            Log.d(TAG, msg);
+    }
+
+    public static void d(String msg) {
+        if (DEBUG)
+            Log.d(TAG, msg);
+    }
+
+    public static void e(String msg) {
+        if (DEBUG)
+            Log.d(TAG, msg);
+    }
+
+    public static void v(String msg) {
+        if (DEBUG)
+            Log.d(TAG, msg);
+    }
+
+    public static void w(String msg) {
+        if (DEBUG)
+            Log.d(TAG, msg);
+    }
+
+    // 自定义tag
+    public static void i(String tag, String msg) {
+        if (DEBUG)
+            Log.d(tag, msg);
+    }
+
+    public static void d(String tag, String msg) {
+        if (DEBUG)
+            Log.d(tag, msg);
+    }
+
+    public static void e(String tag, String msg) {
+        if (DEBUG)
+            Log.d(tag, msg);
+    }
+
+    public static void v(String tag, String msg) {
+        if (DEBUG)
+            Log.d(tag, msg);
+    }
+
+    public static void w(String tag, String msg) {
+        if (DEBUG)
+            Log.d(tag, msg);
+    }
+
+}
